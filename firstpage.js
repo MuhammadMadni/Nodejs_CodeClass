5461d661f4e1e3fcfc230c0af24ab2c64af36b9e
MuhammadMadni <muhammadmadni451@gmail.com>
2019-04-05 11:23:54 +0500

    update Node index page

--- firstpage.js
+++ firstpage.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req,res){
 	res.writeHead(200,{'Content-Type': 'Text/html'});
-    res.end('<h1>Hello Class: This is First Server Page<h1>');
+    res.end('<h1>Hello Class: This is Seconds Server Page<h1>');
 }).listen(8080);

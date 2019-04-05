206229faa4357f3c6ef03aab233049f1aedf6753
MuhammadMadni <muhammadmadni451@gmail.com>
2019-04-05 10:56:39 +0500

    Nodejs Class 5

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Single Page CV</title>
+</head>
+<body>
+    <div> <!--Nav div-->
+
+    <a href="professionalresume.html">HOME</a>|<a href="">EDUCATION </a>|<a href="">EXPERIENCE</a>
+
+<div>	<!-- picture div-->
+<img src="Images/FB_IMG_1545476192404.jpg" />
+
+<div> <!-- Objective div-->
+<h1>Muhammad Madni</h1>
+<h2>0324-9485508</h2>
+<h3>muhammadmadni451@gmail.com</h3>
+<h2>Objective:</h2>
+<p>Long Time Consulting Project or Permanent position</p>
+
+<div> <!--Personal Info-->
+	<h2>Personal Information:</h2>
+	<p>Father Name:Sareer Ahmad kamal </p>
+	<p>Contact No:0324-9485508</p> 
+	<p>muhammadmadi451@gmail.com</p>
+	<p>House no'D' Rehman Block Rizwan garden Near Sozo Water Park Lahore Cannt</p>
+
+<div> <!-- Education Div-->
+	<h2>Education:</h2> 
+	<p><a href="Education/matric.html">.Matric</a></p>
+	<p><a href=" Education/fsc.html">.ICS </a></p>
+	<p><a href="Education/bscs.html">.BSCS(Cont..)</a></p>
+
+<div> <!--Experiences Div-->
+	<h2>Experiences:</h2>
+	<p><a href="Experiences/Arbisoft.html">.Arbisoft</a></p>
+	<p><a href="Experiences/abacus.html">.ABACUS</a></p>
+	<p><a href="Experiences/mindstormstudios.html">.Mindstorm Studios</a></p>
+<div> <!--Hobbies Div-->
+   <h2>Hobbies:</h2>
+   <p>.Cycling</p>
+   <p>.Cricket</p>
+   <p>.Movies</p>
+   <div> <!--Skills div-->
+   	<h2>Skills:</h2>
+   	<p>Time Management</p>
+   	<p>Hard worker</p>
+   	<p>Punctual</p>
+   	<p>Good decision</p>
+
+
+ </div>  <!--End Skills Div--> 
+</div>  <!--End Hobbies-->
+</div>   <!--End Experiences-->
+</div>   <!--End Education div-->
+</div>   <!--End Personal info-->
+</div>  <!-- End Objective Div-->
+</div>  <!-- End picture div-->
+</div>  <!--End Nav div-->
+
+
+
+</body>
+</html>
